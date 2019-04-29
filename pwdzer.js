f90f019fc0d543c9c8c25708138bc56377839c70
jugonzal <jugonzal@gmail.com>
2019-04-29 16:02:27 -0400

    password length check

--- pwdzer.js
+++ pwdzer.js
@@ -1,11 +1,21 @@
 // we should ask for a password
 
 var password = process.argv[2]
+var goodPassword = true;
 
-console.log(password)
+console.log("Password to be analyzed: ",password)
 
 // - Make sure your password is long enough: 9 characters
+
+if (password.length >= 9) {
+	console.log("Password is long enough.")
+} else {
+	console.log("Bad Password. Not long enough.");
+	goodPassword = false;
+}
+
 // - It has upper case and lower case
+
 // - it has special symbols
 // - it has numbers
 // - not common words (i.e. NOT "password")
