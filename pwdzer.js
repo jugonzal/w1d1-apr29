5a83640e898500d4c4538ba10019b67f97355775
jugonzal <jugonzal@gmail.com>
2019-04-29 15:56:03 -0400

    got the password from the console

---
+++ pwdzer.js
@@ -0,0 +1,12 @@
+// we should ask for a password
+
+var password = process.argv[2]
+
+console.log(password)
+
+// - Make sure your password is long enough: 9 characters
+// - It has upper case and lower case
+// - it has special symbols
+// - it has numbers
+// - not common words (i.e. NOT "password")
+// - if not strong enough suggest a better one.
